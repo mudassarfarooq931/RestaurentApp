66c88adb717d8a81a22eaabffc1b47d80d50ddfc
mudassar1918 <mudassar@hawklogix.com>
2024-09-23 22:21:47 +0500

    create new branch development

--- src/screens/login-screen/login-screen.tsx
+++ src/screens/login-screen/login-screen.tsx
@@ -93,7 +93,7 @@ const LoginScreen = memo(({}: IProps) => {
         <View style={styles.form}>
           <View style={styles.formHeader}>
             {/* <Image source={require('@images/ABHA.png')} style={styles.logo} /> */}
-            <Text style={styles.headerText}>Signup </Text>
+            <Text style={styles.headerText}>Signup</Text>
           </View>
 
           <Input
